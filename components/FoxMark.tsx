bc6cff555ebcb16dd329872f13f16b54096815f0
wesamHab <94143760+wesamHab@users.noreply.github.com>
2026-09-08 18:41:40 +0200

    Refine KitsuWire fox brand mark

--- components/FoxMark.tsx
+++ components/FoxMark.tsx
@@ -1,12 +1,2 @@
-type FoxMarkProps = { size?: number; className?: string };
-
-export function FoxMark({ size = 34, className = "" }: FoxMarkProps) {
-  return (
-    <svg className={className} width={size} height={size} viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-      <path d="M12 17L25 22L32 12L39 22L52 17L47 38C45 47 39 53 32 55C25 53 19 47 17 38L12 17Z" fill="currentColor"/>
-      <path d="M20 27L27 31L23 36L20 27Z" fill="#C9FF42"/>
-      <path d="M44 27L37 31L41 36L44 27Z" fill="#C9FF42"/>
-      <path d="M27 42L32 46L37 42L32 50L27 42Z" fill="#C9FF42"/>
-    </svg>
-  );
-}
+type FoxMarkProps={size?:number;className?:string};
+export function FoxMark({size=34,className=""}:FoxMarkProps){return <svg className={className} width={size} height={size} viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"><path d="M10 14L24 21L32 9L40 21L54 14L49 37C47 47 40 54 32 57C24 54 17 47 15 37L10 14Z" fill="#0B0D0F"/><path d="M13 18L24 24L18 31L13 18Z" fill="#B9F33E"/><path d="M51 18L40 24L46 31L51 18Z" fill="#B9F33E"/><path d="M22 31L29 34L25 39L22 31Z" fill="white"/><path d="M42 31L35 34L39 39L42 31Z" fill="white"/><path d="M27 44L32 48L37 44L32 53L27 44Z" fill="#B9F33E"/></svg>}
