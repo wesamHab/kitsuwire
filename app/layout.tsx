17c2c51c221dea2a99e446748ee45e8d8fff1cf7
wesamHab <94143760+wesamHab@users.noreply.github.com>
2026-09-09 14:18:26 +0200

    Load system health admin styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ import "./admin.css";
 import "./media.css";
 import "./seo-admin.css";
 import "./taxonomy-admin.css";
+import "./system-admin.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://kitsuwire.com"),
